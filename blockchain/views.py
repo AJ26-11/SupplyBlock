8b06fcdc9e85e317b0bf67cbf37118b760b682b0
Adithya <AJ26-11>
2023-11-30 19:16:11 +0530

    authentication views updated

--- blockchain/views.py
+++ blockchain/views.py
@@ -211,3 +211,31 @@ def view_batch_details(request):
     return render(request, 'view_batch_details.html')
 
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            # Return an 'invalid login' error message
+            pass
+    return render(request, 'login.html')
+
+
