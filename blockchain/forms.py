843d75084ec75b45b193b1e1b03c2c68d53ba1ab
Aryan Bhasein <aryanbhasein1@gmail.com>
2023-12-02 01:11:55 +0530

    Updated models and forms

--- blockchain/forms.py
+++ blockchain/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from .models import CoffeeBeanBatch
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
 
 class AddBatchForm(forms.ModelForm):
     class Meta:
--- blockchain/models.py
+++ blockchain/models.py
@@ -1,6 +1,14 @@
 
 from django.db import models
 
+
+class Batch(models.Model):
+    batch_id = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.batch_id
+
+
 class CoffeeBeanBatch(models.Model):
     batch_id = models.CharField(max_length=255, unique=True)
     farm_name = models.CharField(max_length=255)
