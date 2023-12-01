51f5176aec4eed185ed8c509abd08983f1745e2a
jrap-bit <namanpandey10@gmail.com>
2023-12-02 01:41:45 +0530

    Added Static Root

--- supplyblocks/settings.py
+++ supplyblocks/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
