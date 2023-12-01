c07046e7e6998554ee67640830b445d983be2bea
jrap-bit <namanpandey10@gmail.com>
2023-12-02 00:29:05 +0530

    Views Removed Forms

--- blockchain/views.py
+++ blockchain/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from web3 import Web3
-from .forms import RegisterForm
+import datetime
 
 # Ethereum setup
 infura_url = 'https://sepolia.infura.io/v3/069ed309e7484022918cfca9a6d772f3'
@@ -18,10 +18,10 @@ contract_address_bytes = web3.to_checksum_address(contract_address)
 # Replace 'YOUR_CONTRACT_ABI' with the ABI (Application Binary Interface) of your smart contract
 contract_abi = [
     {
-        "anonymous": false,
+        "anonymous": False,
         "inputs": [
             {
-                "indexed": false,
+                "indexed": False,
                 "internalType": "string",
                 "name": "batchId",
                 "type": "string"
@@ -31,16 +31,16 @@ contract_abi = [
         "type": "event"
     },
     {
-        "anonymous": false,
+        "anonymous": False,
         "inputs": [
             {
-                "indexed": false,
+                "indexed": False,
                 "internalType": "string",
                 "name": "batchId",
                 "type": "string"
             },
             {
-                "indexed": false,
+                "indexed": False,
                 "internalType": "string",
                 "name": "finalLocation",
                 "type": "string"
@@ -50,22 +50,22 @@ contract_abi = [
         "type": "event"
     },
     {
-        "anonymous": false,
+        "anonymous": False,
         "inputs": [
             {
-                "indexed": false,
+                "indexed": False,
                 "internalType": "string",
                 "name": "batchId",
                 "type": "string"
             },
             {
-                "indexed": false,
+                "indexed": False,
                 "internalType": "string",
                 "name": "packagingDetails",
                 "type": "string"
             },
             {
-                "indexed": false,
+                "indexed": False,
                 "internalType": "uint256",
                 "name": "packagingDate",
                 "type": "uint256"
@@ -75,22 +75,22 @@ contract_abi = [
         "type": "event"
     },
     {
-        "anonymous": false,
+        "anonymous": False,
         "inputs": [
             {
-                "indexed": false,
+                "indexed": False,
                 "internalType": "string",
                 "name": "batchId",
                 "type": "string"
             },
             {
-                "indexed": false,
+                "indexed": False,
                 "internalType": "string",
                 "name": "processingDetails",
                 "type": "string"
             },
             {
-                "indexed": false,
+                "indexed": False,
                 "internalType": "uint256",
                 "name": "roastingDate",
                 "type": "uint256"
@@ -100,16 +100,16 @@ contract_abi = [
         "type": "event"
     },
     {
-        "anonymous": false,
+        "anonymous": False,
         "inputs": [
             {
-                "indexed": false,
+                "indexed": False,
                 "internalType": "string",
                 "name": "batchId",
                 "type": "string"
             },
             {
-                "indexed": false,
+                "indexed": False,
                 "internalType": "string",
                 "name": "newLocation",
                 "type": "string"
@@ -381,6 +381,59 @@ private_key = "6214ca32dbc6e81217d6bcdd1f75c618691bc7e7943637d6202b182fed5e2d9d"
 contract = web3.eth.contract(address=contract_address_bytes, abi=contract_abi)
 
 
+def convert_date_to_unix(date_str):
+    """Convert date string to UNIX timestamp."""
+    try:
+        dt = datetime.strptime(date_str, "%Y-%m-%d")
+        return int(time.mktime(dt.timetuple()))
+    except ValueError:
+        return 0  # Handle invalid date format
+
+
+def send_transaction(contract_function, *args):
+    transaction = contract_function(*args).build_transaction({
+        'chainId': 1337,
+        'gas': 2000000,
+        'gasPrice': web3.to_wei('50', 'gwei'),
+        'nonce': web3.eth.get_transaction_count(web3.to_checksum_address(account)),
+    })
+
+    signed_txn = web3.eth.account.sign_transaction(transaction, private_key)
+    return web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+
+
+def add_batch(request):
+    if request.method == 'POST':
+        batch_id = request.POST['batch_id']
+        farm_name = request.POST['farm_name']
+        origin_country = request.POST['origin_country']
+        harvest_date = convert_date_to_unix(request.POST['harvest_date'])
+        processing_details = request.POST['processing_details']
+        roasting_date = convert_date_to_unix(request.POST['roasting_date'])
+        packaging_details = request.POST['packaging_details']
+        packaging_date = convert_date_to_unix(request.POST['packaging_date'])
+        is_shipped = 'is_shipped' in request.POST
+        is_delivered = 'is_delivered' in request.POST
+        current_location = request.POST['current_location']
+
+        # Send the transaction
+        tx_hash = send_transaction(contract.functions.addBatch,
+                                   batch_id,
+                                   farm_name,
+                                   origin_country,
+                                   harvest_date,
+                                   processing_details,
+                                   roasting_date,
+                                   packaging_details,
+                                   packaging_date,
+                                   is_shipped,
+                                   is_delivered,
+                                   current_location)
+        return render(request, 'success.html', {'tx_hash': tx_hash.hex()})
+
+    return render(request, 'add_batch.html')
+
+
 def store_data(request):
     if request.method == 'POST':
         item_id = request.POST['item_id']
@@ -414,7 +467,8 @@ def verify_data(request):
         # Check data integrity using the smart contract's verifyItem function
         is_verified = contract.functions.verifyItem(item_id, item_data).call()
 
-        return render(request, 'verification_result.html', {'item_id': item_id, 'item_data': item_data, 'is_verified': is_verified})
+        return render(request, 'verification_result.html',
+                      {'item_id': item_id, 'item_data': item_data, 'is_verified': is_verified})
 
     return render(request, 'verify_data.html')
 
@@ -430,6 +484,7 @@ def view_items(request):
 
     return render(request, 'view_items.html', {'items': items})
 
+
 def process_batch(request):
     if request.method == 'POST':
         batch_id = request.POST['batch_id']
@@ -440,6 +495,7 @@ def process_batch(request):
         return render(request, 'success.html', {'tx_hash': tx_hash.hex()})
 
     return render(request, 'process_batch.html')
+
 
 def package_batch(request):
     if request.method == 'POST':
@@ -452,6 +508,7 @@ def package_batch(request):
 
     return render(request, 'package_batch.html')
 
+
 def ship_batch(request):
     if request.method == 'POST':
         batch_id = request.POST['batch_id']
@@ -463,7 +520,6 @@ def ship_batch(request):
     return render(request, 'ship_batch.html')
 
 
-
 def deliver_batch(request):
     if request.method == 'POST':
         batch_id = request.POST['batch_id']
@@ -473,6 +529,7 @@ def deliver_batch(request):
         return render(request, 'success.html', {'tx_hash': tx_hash.hex()})
 
     return render(request, 'deliver_batch.html')
+
 
 def view_batch_details(request):
     if request.method == 'POST':
@@ -511,21 +568,3 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'login.html', {'form': form})
-
-
-def add_batch(request):
-    if request.method == 'POST':
-        try:
-            batch_id = request.POST['batch_id']
-            farm_name = request.POST['farm_name']
-            origin_country = request.POST['origin_country']
-            harvest_date = request.POST['harvest_date']
-
-            tx_hash = send_transaction(contract.functions.addBatch, batch_id, farm_name, origin_country, int(harvest_date))
-            return render(request, 'success.html', {'tx_hash': tx_hash.hex()})
-        except Exception as e:
-            # Handle exception (e.g., log it, show an error message)
-            return render(request, 'add_batch.html', {'error': str(e)})
-
-    return render(request, 'add_batch.html')
-
