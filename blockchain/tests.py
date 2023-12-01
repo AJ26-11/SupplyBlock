091b8b81016437784f73ff47e76e69b9c47851f3
Aryan Bhasein <aryanbhasein1@gmail.com>
2023-12-02 00:14:16 +0530

    Added basic tests

--- blockchain/tests.py
+++ blockchain/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import CoffeeBeanBatch
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class CoffeeBeanBatchModelTests(TestCase):
+
+    def setUp(self):
+        CoffeeBeanBatch.objects.create(
+            batch_id="batch123",
+            farm_name="Test Farm",
+            origin_country="Testland",
+            harvest_date="2021-01-01",
+            # ... other fields ...
+        )
+
