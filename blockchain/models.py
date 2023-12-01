19215935aa5d59bc7159d93bf556d987162a40b0
Aryan Bhasein <aryanbhasein1@gmail.com>
2023-12-02 01:16:27 +0530

    Removed forms and models

--- blockchain/models.py
+++ blockchain/models.py
@@ -8,19 +8,3 @@ class Batch(models.Model):
     def __str__(self):
         return self.batch_id
 
-
-class CoffeeBeanBatch(models.Model):
-    batch_id = models.CharField(max_length=255, unique=True)
-    farm_name = models.CharField(max_length=255)
-    origin_country = models.CharField(max_length=255)
-    harvest_date = models.DateField()
-    processing_details = models.TextField(blank=True)
-    roasting_date = models.DateField(null=True, blank=True)
-    packaging_details = models.TextField(blank=True)
-    packaging_date = models.DateField(null=True, blank=True)
-    is_shipped = models.BooleanField(default=False)
-    is_delivered = models.BooleanField(default=False)
-    current_location = models.CharField(max_length=255, blank=True)
-
-    def __str__(self):
-        return self.batch_id
