0877e648aa43177eddb04cea6b0d6cff66cb8d8d
jrap-bit <namanpandey10@gmail.com>
2023-12-02 01:09:45 +0530

    Added Ganache Support

--- blockchain/deploy_contract.py
+++ blockchain/deploy_contract.py
@@ -1,10 +1,10 @@
 from web3 import Web3, HTTPProvider
 from solcx import compile_source
 
-# Set up Ethereum connection
-web3 = Web3(HTTPProvider("https://sepolia.infura.io/v3/069ed309e7484022918cfca9a6d772f3"))
+# web3 = Web3(HTTPProvider("https://sepolia.infura.io/v3/069ed309e7484022918cfca9a6d772f3"))
+web3 = Web3(HTTPProvider("http://127.0.0.1:7545"))
 
-# Compile the smart contract source code
+
 contract_source_code = """
 pragma solidity ^0.8.0;
 
@@ -87,14 +87,15 @@ contract CoffeeBeanSupplyChain {
 """
 
 compiled_sol = compile_source(contract_source_code)
-contract_interface = compiled_sol['<stdin>:SupplyChain']
+contract_interface = compiled_sol['<stdin>:CoffeeBeanSupplyChain']
 
 print("Contract ABI:")
 print(contract_interface['abi'])
 
-# Deploy the smart contract
-account = "0xa7255745188F75a23b6e241c69D360AE606B2da2"
-private_key = "6214ca32dbc6e81217d6bcdd1f75c618691bc7e7943637d6202b182fed5e2d9d"
+# account = "0xa7255745188F75a23b6e241c69D360AE606B2da2"
+# private_key = "6214ca32dbc6e81217d6bcdd1f75c618691bc7e7943637d6202b182fed5e2d9d"
+account = "0x5A5cE81e835ce18DCaF8CD24CeeE9815dF79a2eC"
+private_key = "0x050e639bf8cd717bfe34e7271baafd9f304bbc68ec585d821d5075b14efd7c6c"
 
 checksum_address = web3.to_checksum_address(account)
 nonce = web3.eth.get_transaction_count(checksum_address)
@@ -104,18 +105,21 @@ web3.eth.default_account = web3.to_checksum_address(account)
 MyContract = web3.eth.contract(abi=contract_interface['abi'], bytecode=contract_interface['bin'])
 
 transaction_data = MyContract.constructor().build_transaction({
-    'gas': 300000,
-    'gasPrice': web3.to_wei('20', 'gwei'),
+    'gas': 3000000,
+    'gasPrice': web3.to_wei('50', 'gwei'),
     'nonce': nonce,
 })
 
-private_key_bytes = bytes.fromhex(private_key)
+private_key_bytes = private_key
 signed_transaction = web3.eth.account.sign_transaction(transaction_data, private_key_bytes)
 account_bytes = bytes.fromhex(account[2:])
 tx_hash = web3.eth.send_raw_transaction(signed_transaction.rawTransaction)
-tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
+print(f"Transaction sent: {tx_hash.hex()}")
+print(tx_hash)
+tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash, timeout=300)
 contract_address = tx_receipt['contractAddress']
 print(f"Contract deployed at address: {contract_address}")
+
 
 
 
